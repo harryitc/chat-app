a659ae70b84147e7a682b5911165a0b185a05934
Phan Ngọc Cường <cuongharryit@gmail.com>
2024-12-17 21:33:28 +0700

    Feat: new form group creator

---
+++ WindowsFormsApp1/frm_GroupCreator.Designer.cs
@@ -0,0 +1,152 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class frm_GroupCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGroupName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMembers = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(10, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Create a new Group";
+            // 
+            // txtGroupName
+            // 
+            this.txtGroupName.Font = new System.Drawing.Font("Times New Roman", 9.75F);
+            this.txtGroupName.Location = new System.Drawing.Point(10, 49);
+            this.txtGroupName.Name = "txtGroupName";
+            this.txtGroupName.Size = new System.Drawing.Size(146, 22);
+            this.txtGroupName.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Italic);
+            this.label2.Location = new System.Drawing.Point(10, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Group Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Italic);
+            this.label3.Location = new System.Drawing.Point(10, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Members";
+            // 
+            // txtMembers
+            // 
+            this.txtMembers.Font = new System.Drawing.Font("Times New Roman", 9.75F);
+            this.txtMembers.Location = new System.Drawing.Point(10, 99);
+            this.txtMembers.Name = "txtMembers";
+            this.txtMembers.Size = new System.Drawing.Size(146, 22);
+            this.txtMembers.TabIndex = 1;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreate.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnCreate.ForeColor = System.Drawing.Color.Maroon;
+            this.btnCreate.Location = new System.Drawing.Point(26, 207);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(146, 23);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F);
+            this.textBox1.Location = new System.Drawing.Point(10, 149);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Italic);
+            this.label4.Location = new System.Drawing.Point(9, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Miêu tả (tùy chọn)";
+            // 
+            // frm_GroupCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 259);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtMembers);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtGroupName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_GroupCreator";
+            this.Text = "Create a New Grou[";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtGroupName;
+        private Label label2;
+        private Label label3;
+        private TextBox txtMembers;
+        private Button btnCreate;
+        private TextBox textBox1;
+        private Label label4;
+    }
+}
