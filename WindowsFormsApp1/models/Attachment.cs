55bb2e674a6bfcf785433e849d0f066a4872bfb2
Phan Ngọc Cường <cuongharryit@gmail.com>
2024-12-17 23:29:40 +0700

    git commit -m "Feat: add entity framework"

---
+++ WindowsFormsApp1/models/Attachment.cs
@@ -0,0 +1,24 @@
+namespace WindowsFormsApp1.models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Attachment
+    {
+        public int AttachmentID { get; set; }
+
+        public int MessageID { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string FilePath { get; set; }
+
+        [StringLength(50)]
+        public string FileType { get; set; }
+
+        public virtual GroupMessage GroupMessage { get; set; }
+    }
+}
