f3460880385bc66d6511922efcaf74fb08255c51
Phan Ngọc Cường <cuongharryit@gmail.com>
2024-12-22 19:36:32 +0700

    Docs: update comment

--- Comunicator/EventType.cs
+++ Comunicator/EventType.cs
@@ -8,11 +8,11 @@ namespace Comunicator
 {
     public static class EventType
     {
-        public const string SEND_MESSAGE = "SEND_MESSAGE";
-        public const string CREATE_GROUP = "CREATE_GROUP";
-        public const string JOIN_GROUP = "JOIN_GROUP";
-        public const string FRIENDSHIPS = "FRIENDSHIPS";
-        public const string STATUS_ACCOUNT = "STATUS_ACCOUNT";
+        public const string SEND_MESSAGE = "SEND_MESSAGE"; // Truyền tin nhắn (text, image, ...)
+        public const string CREATE_GROUP = "CREATE_GROUP"; // Tạo group
+        public const string JOIN_GROUP = "JOIN_GROUP"; // Tham gia group
+        public const string FRIENDSHIPS = "FRIENDSHIPS"; // Lời mời kết bạn
+        public const string STATUS_ACCOUNT = "STATUS_ACCOUNT"; // Login/ Logout
         // Bổ sung thêm (nếu cần thiết)...
     }
 }
