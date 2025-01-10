5a27db3b1bcb06e06bd49621a1f5fdba39d1ca87
Phan Ngọc Cường <cuongharryit@gmail.com>
2025-01-10 20:10:11 +0700

    Fix: start mistake program

--- Client/Program.cs
+++ Client/Program.cs
@@ -16,7 +16,7 @@ namespace Client
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_Report());
+            Application.Run(new frm_Login());
         }
 
     }
